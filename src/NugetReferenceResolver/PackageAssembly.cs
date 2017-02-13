6fb82c0d02b0bbf0ff88028a79a9e92ff7e2aab9
jacalvar <jacalvar@microsoft.com>
2017-02-13 11:25:27 -0800

    Introduce library to support ApiCheck and Optimization Cache on MSBuild

---
+++ src/NugetReferenceResolver/PackageAssembly.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NugetReferenceResolver
+{
+    public class PackageAssembly
+    {
+        public PackageAssembly(string relativePath, string resolvedPath)
+        {
+            RelativePath = relativePath;
+            ResolvedPath = resolvedPath;
+        }
+
+        public string FileName => Path.GetFileName(ResolvedPath);
+        public string RelativePath { get; set; }
+        public string ResolvedPath { get; set; }
+    }
+}
---
+++ src/NugetReferenceResolver/RuntimeDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NugetReferenceResolver
+{
+    public class RuntimeDefinition
+    {
+        public string Name { get; set; }
+        public IEnumerable<RuntimeDefinition> Fallbacks { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
